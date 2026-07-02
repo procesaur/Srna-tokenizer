2b513cbaa675dac3ce9a35d8ad3c7d24b24fddb6
procesaur <31268468+procesaur@users.noreply.github.com>
2026-07-02 14:48:11 +0200

    inline dict

--- tokenization_test.py
+++ tokenization_test.py
@@ -4,12 +4,13 @@ from tokenization_srna import SrnaTokenizer
 tokenizer = SrnaTokenizer.from_pretrained("procesaur/Srna_test", trust_remote_code=True)
 #tokenizer.omit_tags=True
 text = "Pitao sam se 'да ли ће ме и рођени дјед и оџак надживети'?"
-
+# text = "This is a test 😊\nI was born in 92000, and this is falsé.\n生活的真谛是\nHi  Hello\nHi   Hello\n\n \n  \n Hello\n<s>\nhi<s>there\nThe following string should be properly encoded: Hello.\nBut ird and ปี   ird   ด\nHey how are you doing"
 # Tokenize and print tokens
 tokens = tokenizer.tokenize(text)
 print("Tokens:", tokens)
 
 # Encode to ids and decode back to string
 ids = tokenizer.encode(text)
+print(ids)
 decoded = tokenizer.decode(ids)
 print("Decoded:", decoded)
